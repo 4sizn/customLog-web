b77a50b516d3e69060ea98786ad985c8af9728f5
hsshin <hsshin@rsupport.com>
2022-02-23 01:07:56 +0900

    chore: wip

---
+++ src/__test__/app.test.ts
@@ -0,0 +1,9 @@
+describe("before activate custom log", () => {
+  it("console.log()", () => {
+    console.log("hello World");
+  });
+});
+
+describe("initialize  custom log", () => {});
+
+describe("after custom log", () => {});
