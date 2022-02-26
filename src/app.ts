3720ba9a7a71eae949b263ac9b9c7e62fdaf1848
4sizn <4sizn@naver.com>
2022-02-26 21:08:15 +0900

    chore: packages

---
+++ src/app.ts
@@ -0,0 +1,22 @@
+import { customLog } from "./index";
+import { cssText } from "./sample";
+
+console.log("custom Log 동작 전");
+// customLog.init();
+customLog.init({
+	prefix: "root",
+	timestamp: true,
+	hello: {
+		style: cssText.sample2,
+	},
+});
+
+// console.log("custom Log 동작 후");
+console.log("안녕하세요", { a: "asdf" });
+console.log("로그입니다");
+console.warn("wran입니다");
+console.info("info입니다");
+console.error("error입니다");
+customLog.end();
+console.warn("wran입니다");
+console.info("info입니다");
