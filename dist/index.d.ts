0d42bf96ec0e029ca133efba3818a10d3411fdb2
hsshin <hsshin@rsupport.com>
2022-03-13 01:35:28 +0900

    chore: index.d.ts

---
+++ dist/index.d.ts
@@ -0,0 +1,8 @@
+export declare const customLog: {
+    run: boolean;
+    monkeyConsole: Console;
+    options: RootOption;
+    message: (fn: Function) => (opts: Option) => () => void;
+    init: (options?: RootOption) => void;
+    end: () => void;
+};
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-const path = require("path");
-const { defineConfig } = require("vite");
+// const path = require("path");
+import path from "path";
+import { defineConfig } from "vite";
+import typescript from "@rollup/plugin-typescript";
 
 module.exports = defineConfig({
+	optimizeDeps: {
+		exclude: ["src/__test__", "src/sample.ts", "src/app.ts"],
+	},
+	plugins: [typescript()],
 	build: {
 		lib: {
 			entry: path.resolve(__dirname, "src/index.ts"),
